f9f7209ccb980cc6545789b244a10f9a7c6a4adb
Marllon Drion <marllondrion@gmail.com>
2020-10-16 02:00:43 +1100

    Creating the front-end (Web)

---
+++ web/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Routes from './routes';
+
+import './styles/global.css';
+
+function App() {
+  return <Routes></Routes>;
+}
+
+export default App;
---
+++ web/src/routes.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
+import OrphanagesMap from './pages/OrphanageMap';
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch> 
+        <Route path='/' exact component={Landing}></Route>
+        <Route path='/app' component={OrphanagesMap}></Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
